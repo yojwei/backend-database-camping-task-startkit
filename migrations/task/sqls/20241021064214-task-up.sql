0a9d78ee376801fdcf02df731dc61e7503a12c54
yojwei <yojwei125@gmail.com>
2024-11-27 13:42:50 +0800

    task 2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -46,11 +46,34 @@ SELECT * FROM "USER" limit 3;
     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
+INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
+('7 堂組合包方案',7, 1400),
+('14 堂組合包方案',14, 2520),
+('21 堂組合包方案',7, 4800);
 
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
+(
+(SELECT id FROM "USER" WHERE name = '王小明'),
+(SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案')
+),
+(
+(SELECT id FROM "USER" WHERE name = '王小明'),
+(SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案')
+),
+(
+(SELECT id FROM "USER" WHERE name = '好野人'),
+(SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案')
+);
 
 
 -- ████████  █████   █    ████   
